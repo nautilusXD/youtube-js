6de741c2db51cee5fafb1c0a4a6aa79af7d11740
Samuel Espinoza <samuelesponline@gmail.com>
2021-08-24 23:04:29 -0500

    spa
    24 agosto

--- spa/app/App.js
+++ spa/app/App.js
@@ -2,17 +2,24 @@ import { ajax } from "./helpers/ajax.js";
 import api from "./helpers/wp_api.js";
 import { Loader } from "./components/Loader.js";
 import { Header } from "./components/Header.js";
+import { Posts } from "./components/Posts.js";
+import { PostCard } from "./components/PostCard.js";
 
 export function App() {
     const d = document,
         $root = d.getElementById("root");
     $root.appendChild(Header());
+    $root.appendChild(Posts());
     $root.appendChild(Loader());
+
+    ajax({
+        url: api.POSTS,
+        cbSuccess: (posts) => {
+            let html = "";
+            posts.forEach(post => html += PostCard(post));
+            d.querySelector(".loader").style.display = "none";
+            d.getElementById("posts").innerHTML = html;
+        }
+    });
+
 }
-
-ajax({
-    url: api.POSTS,
-    cbSuccess: (posts) => {
-
-    }
-});
---
+++ spa/app/components/Posts.js
@@ -0,0 +1,6 @@
+export function Posts() {
+    const $posts = document.createElement("section");
+    $posts.classList.add("grid-fluid");
+    $posts.id = "posts";
+    return $posts;
+}
